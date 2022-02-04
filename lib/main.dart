607fd5f4f6fb42dcfb7d09f469278dce1b516790
dillonl <dillonl49@163.com>
2022-02-04 18:13:50 +0800

    feat: # 🧶 完成视频页面数据渲染

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int currentIndex = 1;
-  List pages = [const Home(),const Moive()];
+  List pages = [const Home(), Moive()];
   @override
 
   List<BottomNavigationBarItem> items(int i) {
